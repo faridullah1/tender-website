f8364d5533a4546f7e34e4fc3c7fe6debd67f5d3
Farid ullah <faridullah996@gmail.com>
2022-11-05 19:06:58 +0500

    created other components and setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TendersComponent } from './components/tenders/tenders.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: 'tenders', component: TendersComponent },
+	{ path: 'projects', component: ProjectsComponent },
+	{ path: 'terms', component: TermsComponent },
+	{ path: 'contact_us', component: ContactUsComponent },
 	{ path: 'login', component: LoginComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
+import { TendersComponent } from './components/tenders/tenders.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    TendersComponent,
+    ProjectsComponent,
+    ContactUsComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
