4706af91bec53cf120f3a2f7ff2a3dd326c52c7a
Farid ullah <faridullah996@gmail.com>
2022-12-18 22:04:50 +0500

    projects searching by name

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,9 +1,11 @@
+import { FormControl } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
 import { GenericApiResponse, Project } from 'src/app/models';
 import { ApiService } from 'src/app/services/api.service';
 import { AddProjectComponent } from './add-project/add-project.component';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 
 @Component({
@@ -13,18 +15,26 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 })
 export class ProjectsComponent implements OnInit {
 	projects: Project[] = [];
-	
+	searchFC = new FormControl();
+
 	constructor(private apiService: ApiService,
 				private modalService: BsModalService,
 				private toaster: ToastrService) 
-	{ }
+	{
+		this.searchFC.valueChanges.pipe(debounceTime(400), distinctUntilChanged())
+			.subscribe(search => {
+				this.getProjects(search);
+			})
+	}
 
 	ngOnInit(): void {
 		this.getProjects();
 	}
 
-	getProjects(): void {
-		this.apiService.get('/projects').subscribe({
+	getProjects(search: string | null = null): void {
+		const slug = search ? `/projects?name=${search}` : '/projects';
+
+		this.apiService.get(slug).subscribe({
 			next: (resp: GenericApiResponse) => this.projects = resp.data.projects,
 			error: (error: any) => this.toaster.error(error)
 		});
@@ -33,7 +43,6 @@ export class ProjectsComponent implements OnInit {
 	createProject(): void {
 		const modalRef = this.modalService.show(AddProjectComponent);
 		if (modalRef.content) modalRef.content.modalRef = modalRef;
-		
 
 		modalRef.onHide?.subscribe(resp => this.getProjects());
 	}
